310aca66e3b2a558af6886fac22108d918749d80
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-08-03 04:16:06 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,28 +1,36 @@
+# use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  your_hash = {"myhash" => "hash"}
+end
 
+# set a variable called `the_manifest`, equal to a hash
+# fill that hash with key/value pairs that describe the following information: 
+# We have 5 whale bone corsets, 2  and 3 oil paintings 
   
-end
-
-
 def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-
+  the_manifest = {
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3
+  }
 end
 
+
+# Look up the value of the "oil paintings" key in the shipping_manifest hash below
 def retrieval
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+    "whale bone corsets" => 5, 
+    "porcelain vases" => 2, 
+    "oil paintings" => 3
+  }
+  shipping_manifest["oil paintings"] 
 end
+
+# add 2 muskets to the shipping_manifest hash below
+# add 4 gun powder to the shipping_manifest hash below
+# return the shipping_manifest hash below
+#hash["new_key"] = "New Value"
 
 def adding
   shipping_manifest = {
@@ -30,14 +38,12 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
-
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
+  shipping_manifest["muskets" => 2]
+  shipping_manifest["muskets"]= 2
+  shipping_manifest["gun powder" => 4]
+  shipping_manifest["gun powder"]= 4
+  return shipping_manifest
 end
+
+  
+  
